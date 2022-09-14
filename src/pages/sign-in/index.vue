37439713d9effb3a11c47a54af158cba42e76cd7
l1m2e <1956227985@qq.com>
2022-09-14 21:10:44 +0800

    info

--- src/pages/sign-in/index.vue
+++ src/pages/sign-in/index.vue
@@ -1,18 +1,18 @@
 <script lang="ts" setup>
+import { getUserAtnApi } from '~/api'
 const msg = $ref('hello')
-import { getUserAtnApi } from '~/api';
-const fn = async () =>{
-  const param = {
-    cardID:'20001'
-  }
-  const res = await getUserAtnApi(param)
-  console.log(res);
+const fn = async () => {
+	const param = {
+		cardID: '20001'
+	}
+	const res = await getUserAtnApi(param)
+	console.log(res)
 }
 fn()
 </script>
 
 <template>
-<div btn @click="">{{msg}}</div>
+	<div btn @click="toggleDark()">{{ msg }}</div>
 </template>
 
 <style scoped></style>
